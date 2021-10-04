4d9bab60cdecf95b94115f3695651c7748822894
anthonyqsun <qsun30@stuy.edu>
2021-10-04 16:54:30 -0400

    add 10

---
+++ 10_bigsib/BigSib.java
@@ -0,0 +1,12 @@
+/*
+ * BeepBoop solo: Anthony Sun + Corn
+ * APCS
+ * HW09 -- packages
+ * 2021-10-03
+*/
+package greeter;
+public class BigSib {
+	public static String greet(String name) {
+		return ("Why, hello there, "+name+". How do you do?");
+	}
+}
---
+++ 10_bigsib/Greet.java
@@ -0,0 +1,15 @@
+/*
+ * BeepBoop solo: Anthony Sun + Corn
+ * APCS
+ * HW09 -- packages
+ * 2021-10-03
+*/
+package greeter;
+import greeter.BigSib;
+public class Greet {
+    public static void main( String[] args ) {
+        System.out.println(BigSib.greet("Crosby"));
+        System.out.println(BigSib.greet("Stills"));
+        System.out.println(BigSib.greet("Nash"));
+    }
+}
