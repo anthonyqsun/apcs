93619147f1dda10bdd10befc1b89a42c2c23a2b0
anthonyqsun <qsun30@stuy.edu>
2021-10-05 21:42:23 -0400

    add 12

---
+++ 12_bigsib/BigSib.java
@@ -0,0 +1,26 @@
+/*
+ * Anthony Sun + corn, Nafiz Labib + Martho
+ * APCS
+ * HW12 -- instance methods
+ * 2021-10-05
+ *
+ * DISCOVERIES:
+ * - Static is not needed for instance method
+ * - Generate an object with "new" keyword
+ *
+ * QCC:
+ * - Is there a way to implement differences between different objects of the same class?
+ */
+
+
+public class BigSib {
+	String helloMsg;
+
+	public void setHelloMsg(String msg) {
+		helloMsg = msg;
+	}
+
+	public String greet(String msg) {
+		return helloMsg + " " + msg;
+	}
+}
---
+++ 12_bigsib/Greet.java
@@ -0,0 +1,24 @@
+public class Greet {
+  public static void main( String[] args ) {
+    String greeting;
+
+    BigSib richard = new BigSib(); // create BigSib obj
+    
+    richard.setHelloMsg("Word up");
+    greeting = richard.greet("freshman");
+    System.out.println(greeting);
+
+    richard.setHelloMsg("Salutations");
+    greeting = richard.greet("Dr. Spaceman");
+    System.out.println(greeting);
+
+    richard.setHelloMsg("Hey ya");
+    greeting = richard.greet("Kong Fooey");
+    System.out.println(greeting);
+
+    richard.setHelloMsg("Sup");
+    greeting = richard.greet("mom");
+    System.out.println(greeting);
+
+  } //end main()
+} //end Greet
