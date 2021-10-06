f41b9a54639ba3f82aff0d6c30bb533a153629e2
anthonyqsun <qsun30@stuy.edu>
2021-10-06 12:02:14 -0400

    prelim 13

---
+++ 13/BigSib.java
@@ -0,0 +1,25 @@
+/*
+ * Anthony Sun + corn, Nafiz Labib + Martha
+ * APCS
+ * HW12 -- instance methods
+ * 2021-10-05
+ *
+ * DISCOVERIES:
+ * - Static is not needed for instance method
+ * - Generate an object with "new" keyword
+ *
+ * QCC:
+ * - Is there a way to implement differences between different objects of the same class?
+ */
+
+
+public class BigSib {
+	String helloMsg;
+	public BigSib(String msg) {
+		helloMsg = msg;
+	}
+
+	public String greet(String msg) {
+		return helloMsg + " " + msg;
+	}
+}
