bd4da7953b594ea5f5730b92291457567f329cf0
anthonyqsun <qsun30@stuy.edu>
2022-04-05 21:12:33 -0400

    add lab08

--- cobo/100_data/DataStudentCode/Welcome03_List.java
+++ cobo/100_data/DataStudentCode/Welcome03_List.java
@@ -9,19 +9,32 @@ import java.util.Scanner;
 public class Welcome03_List {
    public static void main(String[] args) {
       DataSource ds = DataSource.connect("http://weather.gov/xml/current_obs/index.xml").load();
-      ArrayList<WeatherStation> allstns = ds.fetchList("WeatherStation", "station/station_name", 
+      ArrayList<WeatherStation> allstns = ds.fetchList("WeatherStation", "station/station_name",
              "station/station_id", "station/state",
              "station/latitude", "station/longitude");
       System.out.println("Total stations: " + allstns.size());
-      
+
       Scanner sc = new Scanner(System.in);
       System.out.println("Enter a state abbreviation: ");
       String state = sc.next();
       System.out.println("Stations in " + state);
+
+      WeatherStation lowest = allstns.get(0);
       for (WeatherStation ws : allstns) {
          if (ws.isLocatedInState(state)) {
-            System.out.println("  " + ws.getId() + ": " + ws.getName());
+           lowest = ws;
+           break;
+         }}
+      for (WeatherStation ws : allstns) {
+         if (ws.isLocatedInState(state)) {
+            System.out.println("  " + ws.getId() + ": " + ws.getName() + " at latitude: " + ws.getLatitude());
+            if (ws.getLatitude() < lowest.getLatitude()){
+              lowest = ws;
+            }
          }
+
       }
+      System.out.println("The southernmost weather station in " + state + " is " + lowest.getId());
+
    }
 }
